8e41fc181a49bb1ddf00a5e5d54707549134303c
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-23 16:13:36 +0530

    //Define variables to select HTML elements

---
+++ script.js
@@ -0,0 +1,4 @@
+//Define variables to select HTML elements
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
