aa0a55bd8e7a57220ebf3f7ad35e3fbc966d42e7
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-23 16:15:09 +0530

    Function to check if a message contains spam

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
 const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
 const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
+
+// Create a denyList array with regular expressions
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
+
+// Function to check if a message contains spam
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
